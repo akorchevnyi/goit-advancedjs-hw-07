cd711fe48da27015b1e6fb3ad3f67798db7d2a00
Andrii Korchevnyi <andrey.korchevnyi@pragmaticdlt.com>
2024-11-02 11:31:45 +0200

    redy

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,75 @@
+// region Interfaces
+interface IKey {
+    getSignature(): number;
+}
+
+interface IPerson {
+    getKey(): number;
+}
+
+interface IHouse {
+    comeIn(tenant: IPerson): void;
+
+    openDoor(key: number): void;
+}
+
+interface IMyHouse {
+    openDoor(someKey: number): void;
+}
+
+// endregion Interfaces
+
+
+// region Classes
+class Key implements IKey {
+    private readonly signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    public getSignature(): number {
+        return this.signature;
+    }
+}
+
+
+class Person implements IPerson {
+    constructor(private key: IKey) {}
+
+    public getKey(): number {
+        return this.key.getSignature();
+    }
+}
+
+
+abstract class House implements IHouse {
+
+    protected constructor(
+        protected key: IKey,
+        protected door: boolean           = false,
+        protected tenants: Array<IPerson> = []) {}
+
+    public comeIn(tenant: IPerson) {
+        if (this.door) this.tenants.push(tenant);
+    }
+
+    abstract openDoor(someKey: number): void
+}
+
+
+class MyHouse extends House implements IMyHouse {
+    constructor(key: IKey) {
+        super(key);
+    }
+
+    public openDoor(someKey: number) {
+        this.door = someKey === this.key.getSignature();
+    }
+}
+// endregion Classes
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
